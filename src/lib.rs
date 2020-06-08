8f7e0ce7b0cc703c53e357f80c41cd3f2b2ca8e8
Erik Zscheile <zseri.devel@ytrizja.de>
2020-06-08 21:44:31 +0200

    user code should be able to find out if a specified limit was reached

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,11 @@ This crate assumes the following behavoirs about `AsyncRead/AsyncWrite` implemen
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-fn ret_reduce(ret: Poll<std::io::Result<usize>>) -> Poll<std::io::Result<()>> {
-    ret.map(|x| x.map(|_| ()))
+fn ret_reduce(
+    ret: Poll<std::io::Result<usize>>,
+    reached_limit: bool,
+) -> Poll<std::io::Result<bool>> {
+    ret.map(|x| x.map(|_| reached_limit))
 }
 
 #[derive(Debug)]
@@ -23,10 +26,17 @@ pub struct PollResult {
     pub delta: usize,
 
     /// yielded with the following result
-    pub ret: Poll<std::io::Result<()>>,
+    /// the inner `bool` specifies if some pre-specified limit was reached
+    pub ret: Poll<std::io::Result<bool>>,
 }
 
-pub fn poll_read<I, O>(mut input: Pin<&mut I>, output: &mut O, cx: &mut Context<'_>) -> PollResult
+/// This function tries to read at most `delta_limit` bytes from `input` to `output`.
+pub fn poll_read<I, O>(
+    mut input: Pin<&mut I>,
+    output: &mut O,
+    cx: &mut Context<'_>,
+    delta_limit: usize,
+) -> PollResult
 where
     I: futures_io::AsyncRead,
     O: bytes::BufMut,
@@ -34,7 +44,14 @@ where
     let mut rdbuf = [0u8; 8192];
     let start = output.remaining_mut();
     loop {
-        let buflim = std::cmp::min(rdbuf.len(), output.remaining_mut());
+        let buflim = *[
+            rdbuf.len(),
+            output.remaining_mut(),
+            delta_limit - (start - output.remaining_mut()),
+        ]
+        .iter()
+        .min()
+        .unwrap();
         match input.as_mut().poll_read(cx, &mut rdbuf[..buflim]) {
             // if we managed to read something....
             Poll::Ready(Ok(n)) if n != 0 => output.put_slice(&rdbuf[..n]),
@@ -44,7 +61,7 @@ where
             ret => {
                 return PollResult {
                     delta: start - output.remaining_mut(),
-                    ret: ret_reduce(ret),
+                    ret: ret_reduce(ret, buflim == 0),
                 }
             }
         }
@@ -67,7 +84,7 @@ where
             ret => {
                 return PollResult {
                     delta: start - input.remaining(),
-                    ret: ret_reduce(ret),
+                    ret: ret_reduce(ret, !input.has_remaining()),
                 }
             }
         }
